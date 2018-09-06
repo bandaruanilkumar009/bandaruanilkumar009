52f402140e3929438a65bc8a7c8a474b6a181beb
bandaruanilkumar009 <42711222+bandaruanilkumar009@users.noreply.github.com>
2018-09-06 22:38:03 +0530

    Add files via upload

---
+++ case.js
@@ -0,0 +1,18 @@
+var a=4
+switch(a){
+    case 1:
+    console.log(15);
+    break;
+    case 2:
+    console.log(30);
+    break;
+    case 3:
+    console.log(45);
+    break;
+    case 4:
+    console.log(70);
+    break;
+    default:
+    console.log('error');
+    
+}
